540a02bee607df9b749d4a4bed90537cf06275c4
Grant Hansen <132412109+therealchunkyd@users.noreply.github.com>
2026-02-11 22:06:48 +1100

    Create SavingsStats component

---
+++ components/roi/savings-stats.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SavingsStats: React.FC = () => {
+    return (
+        <div>
+            <h1>Savings Statistics</h1>
+            {/* Additional code can be added here */}
+        </div>
+    );
+};
+
+export default SavingsStats;
